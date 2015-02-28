4ae719f2fb9d8901bb82be4be93024be4f22073c
andy <unisno@126.com>
2015-02-28 21:39:28 +0800

    创建了Model类，基本数据载体

---
+++ python-proj/wl-capture/uPktModel.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 *-*
+__author__ = 'andy'
+# create 2015.02.27
+# packet model class
+
+class uPktModel:
+    # init
+    def __init__(self):
+        self.mac = ''
+        self.ssid = ''
+        self.rssi = ''
+        self.stat = ''
+        self.type = ''
+        self.subtype = ''
+        self.pmac = ''
+        self.bssid = ''
+        self.pkttime = ''
+        self.timefrac = ''
+        self.frameproto = ''
+        self.chan = ''
+        self.RecTime = ''
+        self.srcip = 'n/a'
