c20901387df3d2937ae98aa7d72d64e94f4574e6
andy <unisno@126.com>
2015-03-01 00:05:27 +0800

    更新了uPktModel，增加了相应格式的默认值。使用该默认值，可以正确存入数据库。

--- python-proj/wl-capture/uPktModel.py
+++ python-proj/wl-capture/uPktModel.py
@@ -3,20 +3,22 @@ __author__ = 'andy'
 # create 2015.02.27
 # packet model class
 
+import os, sys, time, traceback, datetime
+
 class uPktModel:
     # init
     def __init__(self):
         self.mac = ''
         self.ssid = ''
-        self.rssi = ''
-        self.stat = ''
+        self.rssi = '0'
+        self.stat = '0'
         self.type = ''
         self.subtype = ''
         self.pmac = ''
         self.bssid = ''
-        self.pkttime = ''
-        self.timefrac = ''
+        self.pkttime = str(datetime.datetime.now()) 
+        self.timefrac = '0'
         self.frameproto = ''
         self.chan = ''
-        self.RecTime = ''
+        self.RecTime = str(datetime.datetime.now()) 
         self.srcip = 'n/a'
