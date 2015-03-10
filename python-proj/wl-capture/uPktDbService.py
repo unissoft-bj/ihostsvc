a13cee7c520b1b92459578220f54a816cda033eb
andy <unisno@126.com>
2015-03-11 00:55:05 +0800

    调整了调用sql的语句

--- python-proj/wl-capture/uPktDbService.py
+++ python-proj/wl-capture/uPktDbService.py
@@ -22,7 +22,7 @@ class uPktDbService:
             self.dbf = uDbFactory(self.log)
             self.dbf.open()                
         except Exception as e:
-            log.error(e)
+            self.log.error(e)
             traceback.print_exc()
 
     #def __del__(self):
@@ -30,18 +30,18 @@ class uPktDbService:
     # set db password
     def savepktmodel(self,pm = uPktModel()):
         """create a new record"""
-        query = "INSERT INTO wlpkt (mac, ssid,rssi,stat,type,subtype,pmac,bssid,\
-            pkttime,timefrac,frameproto,chan,RecTime,srcip)\
-            values (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s)"
-        values = (pm.mac, pm.ssid, pm.rssi, pm.stat, pm.type, pm.subtype, \
-                  pm.pmac, pm.bssid, pm.pkttime, pm.timefrac, pm.frameproto, \
-                  pm.chan, pm.RecTime, pm.srcip)
+        query = "INSERT INTO wlpkt (mac,ssid,rssi,stat,type,subtype,pmac,bssid,pkttime,timefrac,frameproto,chan,RecTime,srcip) values(%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s)"
+        values = (pm.mac, pm.ssid, pm.rssi, pm.stat, pm.type, pm.subtype,pm.pmac, pm.bssid, pm.pkttime, pm.timefrac, pm.frameproto, pm.chan, pm.RecTime, pm.srcip)
+        
+        #query = "INSERT INTO wlpkt (mac,ssid,rssi,stat,type,subtype,pmac,bssid,pkttime,timefrac,frameproto,chan,RecTime,srcip) values('00:1b:77:1c:92:55', 'Unis', '-51', '0', '', '0x04', 'ff:ff:ff:ff:ff:ff', 'ff:ff:ff:ff:ff:ff', '2014-02-16 17:29:24', '0.562819000', 'radiotap:wlan', '', '2015-03-11 00:07:51.434000', '192.168.0.128')"
+        #values = None
+        
         self.dbf.execute(query, values)
         #self.dbf.commit()
     
     def savestamodel(self,om = uStaModel()):
         """create a new record"""
-        query = "INSERT INTO wlsta (mac, ssid,rssi,stat,firstseen,lastseen,npacket,rectime,srcip) \
+        query = "INSERT INTO wlsta (mac, ssid,rssi,stat,firstseen,lastseen,npacket,rectime,srcip)\
             values (%s, %s, %s, %s, %s, %s, %s, %s, %s)"
         values = (om.mac, om.ssid, om.rssi, om.stat, om.firstseen, om.lastseen, \
                   om.npacket, om.rectime, om.srcip)
@@ -58,8 +58,8 @@ class uPktDbService:
             "recTime = " + "'" + om.rectime +"' " +\
             "where mac ='" + om.mac + "' and  " + \
             "srcip = '" + om.srcip + "'"
-        values = None
-        self.dbf.execute(query, values)
+        #values = None
+        self.dbf.execute(query)
         #self.dbf.commit() 
 
     def getmacbymacsrcip(self,mac,srcip):
@@ -82,8 +82,8 @@ class uPktDbService:
             stat = '200' " +\
             "where mac ='" + om.mac + "' and  " + \
             "srcip = '" + om.srcip + "'"
-        values = None
-        self.dbf.execute(query, values)
+        #values = None
+        self.dbf.execute(query)
         #self.dbf.commit() 
         
     def execute(self,strsql): 
