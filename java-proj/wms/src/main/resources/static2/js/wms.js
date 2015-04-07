8a3581467c865cbf5af35e33b4af9e59c366633a
ycyn521 <ycyn521@qq.com>
2015-04-07 18:44:19 +0800

    commit3

--- java-proj/wms/src/main/resources/static2/js/wms.js
+++ java-proj/wms/src/main/resources/static2/js/wms.js
@@ -73,12 +73,26 @@ myUIRoute.config(function($stateProvider, $urlRouterProvider) {
             url: "/main",
             templateUrl: "tpls/main.htm",
             controller: function($scope,$http) {
+
                 $http.get("rest/user.json")
                 .success(function(response) {$scope.user = response;})
                 .error(function() {
                     //alert("--3--");
                      window.location.href="#/login"; 
                 });
+
+                $scope.logout = function(){
+                    $http.get("/logout")
+                    .success(function(response){
+                        alert("退出成功");
+                        window.location.href="#/login1";
+                    })
+                    .error(function(){
+                        alert("未知错误");
+                    });
+
+                };
+
             }
         })
 
@@ -133,6 +147,61 @@ myUIRoute.config(function($stateProvider, $urlRouterProvider) {
                     .error(function() {
                         $scope.msg="登陆失败，请重新输入上网码";
                         window.location.href="#/login2";
+                    });
+                    
+                }
+            }
+        })
+        //清理磁盘。。。
+        .state('disk', {
+            url: "/disk",
+            templateUrl: "tpls/disk.htm",
+            controller:function($scope,$http){
+                $scope.formData = {};
+                $scope.msg="-";
+                $http.get("rest/disk.json")
+                .success(function(response){
+                    $scope.msg=response;
+                });
+
+                $scope.cleandisk = function(){
+                     //alert($scope.formData.days);
+                    $http.post("rest/cleandisk.php",$scope.formData)
+                    .success(function(response) {
+                        alert("请求已提交。。。");
+                        window.location.href="#/disk";
+                   
+                    })
+                    .error(function() {
+                        //$scope.msg="，请重新输入上网码";
+                        window.location.href="#/disk";
+                    });
+                    
+                }
+            }
+        })
+
+        //发放上网码
+        .state('shangwangma', {
+            url: "/shangwangma",
+            templateUrl: "tpls/shangwangma.htm",
+            controller:function($scope,$http){
+                $scope.formData = {"userrole":"1","type":"web"};
+                $scope.msg="未生成";
+                
+
+                $scope.create_net_code = function(){                     
+                    console.log($scope.formData.phone);
+                    console.log($scope.formData.userrole);
+                    console.log($scope.formData.type);
+                    $http.post("rest/shangwangma.php",$scope.formData)
+                    .success(function(response) {
+                        alert("上网码生成成功");
+                        $scope.msg=response;
+                    })
+                    .error(function() {
+                        $scope.msg="未知错误，请重新生成";
+                        //window.location.href="#/disk";
                     });
                     
                 }
