53bf6b060813f49e85175ab8faae0f2fce2c9553
michaelyin <gt8442b@yahoo.com>
2015-04-10 17:33:45 -0400

    token class.

--- java-proj/wms/src/main/java/net/wyun/wm/domain/token/Token.java
+++ java-proj/wms/src/main/java/net/wyun/wm/domain/token/Token.java
@@ -1,19 +1,23 @@
 /**
  * 
  */
-package net.wyun.wm.domain;
+package net.wyun.wm.domain.token;
 
 import java.util.Date;
 
 import javax.persistence.Column;
+import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.Table;
 
 /**
  * @author Xuecheng
  *
  */
+@Entity
+@Table(name = "token")
 public class Token {
 	
 	private Long id; //            smallint unsigned  NOT NULL auto_increment primary key,
