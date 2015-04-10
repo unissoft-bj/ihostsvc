845acd0369d69ab119d165c58c1ae8f9401b1053
michaelyin <gt8442b@yahoo.com>
2015-04-10 17:28:45 -0400

    add util sql files

---
+++ java-proj/wms/etc/wms_init.sql
@@ -0,0 +1,46 @@
+call createPermission('PERM_CREATE_ACCOUNTS');
+call createPermission('PERM_READ_ACCOUNTS');
+call createPermission('PERM_UPDATE_ACCOUNTS');
+call createPermission('PERM_DELETE_ACCOUNTS');
+call createPermission('PERM_ADMIN_ACCOUNTS');
+
+call createPermission('PERM_CREATE_RECEPTIONS');
+call createPermission('PERM_READ_RECEPTIONS');
+call createPermission('PERM_UPDATE_RECEPTIONS');
+call createPermission('PERM_DELETE_RECEPTIONS');
+call createPermission('PERM_ADMIN_RECEPTIONS');
+
+call createPermission('PERM_CREATE_TOKEN');
+
+-- Create roles
+
+call createRole('ROLE_USER', @role_user);
+call roleHasPermission(@role_user, 'PERM_READ_ACCOUNTS');
+
+call createRole('ROLE_MANAGER', @role_manager);
+call roleHasPermission(@role_manager, 'PERM_CREATE_ACCOUNTS');
+call roleHasPermission(@role_manager, 'PERM_READ_ACCOUNTS');
+call roleHasPermission(@role_manager, 'PERM_UPDATE_ACCOUNTS');
+call roleHasPermission(@role_manager, 'PERM_DELETE_ACCOUNTS');
+call roleHasPermission(@role_manager, 'PERM_CREATE_TOKEN');
+
+call createRole('ROLE_SALES', @role_sales);
+call roleHasPermission(@role_sales, 'PERM_CREATE_RECEPTIONS');
+call roleHasPermission(@role_sales, 'PERM_READ_RECEPTIONS');
+call roleHasPermission(@role_sales, 'PERM_UPDATE_RECEPTIONS');
+call roleHasPermission(@role_sales, 'PERM_DELETE_RECEPTIONS');
+
+
+call createRole('ROLE_SERVICE', @role_service);
+call roleHasPermission(@role_service, 'PERM_CREATE_RECEPTIONS');
+call roleHasPermission(@role_service, 'PERM_READ_RECEPTIONS');
+call roleHasPermission(@role_service, 'PERM_UPDATE_RECEPTIONS');
+call roleHasPermission(@role_service, 'PERM_DELETE_RECEPTIONS');
+
+-- Create accounts
+
+
+call createAccount(SELECT UUID(), '1382188318', 'manager bootstrap');
+call accountHasRole(@paula, @role_user);
+
+
---
+++ java-proj/wms/etc/wms_util_procedure.sql
@@ -0,0 +1,34 @@
+-- =====================================================================================================================
+-- Procedures
+-- =====================================================================================================================
+
+delimiter //
+
+create procedure createPermission($name varchar(50))
+begin
+    insert into permission (name) values ($name);
+end //
+
+create procedure createRole($name varchar(50), out $id smallint)
+begin
+    insert into role (name) values ($name);
+    set $id := last_insert_id();
+end //
+
+create procedure roleHasPermission($role_id smallint, $perm_name varchar(50))
+begin
+    declare _perm_id int;
+    select id from permission where name = $perm_name into _perm_id;
+    insert into role_permission (role_id, permission_id) values ($role_id, _perm_id);
+end //
+
+create procedure createAccount($id varchar(36), $phone varchar(30), $hint varchar(30))
+begin
+    insert into account (id, phone, hint, create_t) values ($id, $phone, $hint, now());
+    set $id := last_insert_id();
+end //
+
+create procedure accountHasRole($account_id int, $role_id smallint)
+begin
+    insert into account_role (account_id, role_id) values ($account_id, $role_id);
+end //
