c6fab2766d6d427d0ae38a5936890ffb23e6729a
michaelyin <gt8442b@yahoo.com>
2015-04-12 12:26:06 -0400

    add script to initialize a manager in db.

--- java-proj/wms/etc/wms_init.sql
+++ java-proj/wms/etc/wms_init.sql
@@ -39,11 +39,11 @@ call roleHasPermission(@role_service, 'PERM_DELETE_RECEPTIONS');
 
 -- Create accounts
 
-
+-- manager account
 call createAccount('90b1e606-dfd3-11e4-90f4-000c29c5df73', '1382188318', 'manager bootstrap');
--- manager
 call accountHasRole('90b1e606-dfd3-11e4-90f4-000c29c5df73', 2)
-call createMac(1096068950523);
+call createMac(1096068950523, @id);
+call macHasAccount(@id, '90b1e606-dfd3-11e4-90f4-000c29c5df73')
 
 
 
