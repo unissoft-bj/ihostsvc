12b73c36c0e0b57b64e197264c6169692ede9507
michaelyin <gt8442b@yahoo.com>
2015-04-12 09:30:06 -0400

    resource rest controller.

--- java-proj/wms/src/main/java/net/wyun/wm/rest/ResourceController.java
+++ java-proj/wms/src/main/java/net/wyun/wm/rest/ResourceController.java
@@ -1,17 +1,5 @@
 /*
- * Copyright 2014 the original author or authors.
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *      http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
+ * Copyright 2014 Unisoft
  */
 
 package net.wyun.wm.rest;
@@ -19,7 +7,7 @@ package net.wyun.wm.rest;
 import java.util.concurrent.atomic.AtomicLong;
 
 import net.wyun.wm.data.Greeting;
-import net.wyun.wm.data.User;
+import net.wyun.wm.domain.mac.Mac;
 
 import org.springframework.security.web.bind.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -29,13 +17,13 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class ResourceController {
 
-	private static final String template = "Hello, %s!";
+	private static final String template = "欢迎使用Matrix WIFI, %s!";
 	private final AtomicLong counter = new AtomicLong();
 
 	@RequestMapping("/resource")
-	public Greeting greeting(@AuthenticationPrincipal User user) {
-		System.out.println("get resource here: " + user.getName());
-		return new Greeting(counter.incrementAndGet(), String.format(template, user.getName()));
+	public Greeting greeting(@AuthenticationPrincipal Mac mac) {
+		System.out.println("get resource here: " + mac.getMacInString());
+		return new Greeting(counter.incrementAndGet(), String.format(template, mac.getMacInString()));
 	}
 
 }
