5a1038533fdf13f2e5af96b02daf9174ed4442dc
michaelyin <gt8442b@yahoo.com>
2015-04-13 23:58:15 -0400

    rest service to get mac addr.

---
+++ java-proj/wms/src/main/java/net/wyun/wm/rest/MacAddressController.java
@@ -0,0 +1,34 @@
+/**
+ * 
+ */
+package net.wyun.wm.rest;
+
+import javax.servlet.http.HttpServletRequest;
+
+import net.wyun.wm.domain.mac.MacAddressLookupService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author Xuecheng
+ *
+ */
+@RestController
+public class MacAddressController {
+	
+	    @Autowired
+	    MacAddressLookupService lookupSvc;
+	
+		@RequestMapping("/mymac")
+		public String getMyMac(HttpServletRequest req) {
+			
+		    String ipAddress = req.getRemoteAddr();  
+		    String mac = lookupSvc.getMacAddrByIP(ipAddress);   
+			
+			return mac;
+		}
+		
+
+
+}
