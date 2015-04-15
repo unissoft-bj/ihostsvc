b1bc99f62ef334d7264e35711e4b7abad89e7f2e
michaelyin <gt8442b@yahoo.com>
2015-04-15 01:04:15 -0400

    finish rest service at end point /mymac

--- java-proj/wms/src/main/java/net/wyun/wm/rest/MacAddressController.java
+++ java-proj/wms/src/main/java/net/wyun/wm/rest/MacAddressController.java
@@ -6,7 +6,9 @@ package net.wyun.wm.rest;
 import javax.servlet.http.HttpServletRequest;
 
 import net.wyun.wm.domain.mac.MacAddressLookupService;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -18,6 +20,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class MacAddressController {
 	
 	    @Autowired
+	    @Qualifier("macAddrLookupService")
 	    MacAddressLookupService lookupSvc;
 	
 		@RequestMapping("/mymac")
