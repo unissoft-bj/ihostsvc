184a84f990c0de2bdae5dd32d8765c5f2e69b8a2
michaelyin <gt8442b@yahoo.com>
2015-04-15 01:00:22 -0400

    add function to generate mac address.

--- java-proj/wms/src/main/java/net/wyun/wm/domain/mac/MacAddressUtil.java
+++ java-proj/wms/src/main/java/net/wyun/wm/domain/mac/MacAddressUtil.java
@@ -3,6 +3,7 @@
  */
 package net.wyun.wm.domain.mac;
 
+import java.util.Random;
 import java.util.regex.Pattern;
 
 import com.google.common.base.CharMatcher;
@@ -89,5 +90,24 @@ public class MacAddressUtil {
 				b.append('-');
 		}
 		return b.toString();
+	}
+	
+	
+	public static String randomMacAddr(){
+		Random rand = new Random();
+	    byte[] macAddr = new byte[6];
+	    rand.nextBytes(macAddr);
+
+	    macAddr[0] = (byte)(macAddr[0] & (byte)254);  //zeroing last 2 bytes to make it unicast and locally adminstrated
+
+	    StringBuilder sb = new StringBuilder(18);
+	    for(byte b : macAddr){
+
+	        if(sb.length() > 0)
+	            sb.append(":");
+
+	        sb.append(String.format("%02x", b));
+	    }
+	    return sb.toString();
 	}
 }
