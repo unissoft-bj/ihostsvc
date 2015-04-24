3b20724debfe2cd3dce6f299be4ccb11074cbf7f
michaelyin <gt8442b@yahoo.com>
2015-04-24 01:10:34 -0400

    add reception controller to create/update a reception.

---
+++ java-proj/wms/src/main/java/net/wyun/wm/domain/reception/ReceptionRepository.java
@@ -0,0 +1,15 @@
+/**
+ * 
+ */
+package net.wyun.wm.domain.reception;
+
+import org.springframework.data.repository.CrudRepository;
+
+/**
+ * @author Xuecheng
+ *
+ */
+public interface ReceptionRepository extends CrudRepository<Reception, Long>{
+	
+
+}
