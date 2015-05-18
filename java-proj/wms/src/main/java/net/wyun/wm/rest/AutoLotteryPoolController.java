f83d67946252ff506c63eb29617f2bf5f905f5b5
michaelyin <gt8442b@yahoo.com>
2015-05-18 16:33:55 -0400

    add api: 1. set lottery time interval for drawing lottery. 2. get all valid phone numbers for lottery drawing. 3. add phone list.

--- java-proj/wms/src/main/java/net/wyun/wm/rest/AutoLotteryPoolController.java
+++ java-proj/wms/src/main/java/net/wyun/wm/rest/AutoLotteryPoolController.java
@@ -3,8 +3,16 @@
  */
 package net.wyun.wm.rest;
 
+import java.util.List;
+
+import net.wyun.wm.domain.autoshow.LotteryTime;
+import net.wyun.wm.service.AutoLotteryService;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 /**
@@ -16,10 +24,27 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class AutoLotteryPoolController {
 	
+	@Autowired
+	AutoLotteryService autoLotteryService;
 	
 	//@PreAuthorize("hasRole('ROLE_ADMIN')")
 	// get a set of phones
 	//
+	@RequestMapping(value="/lottery/time", method = {RequestMethod.POST})
+	public void setLotteryTimeLimit(@RequestBody LotteryTime lotteryTime){
+		autoLotteryService.setLotteryTime(lotteryTime);
+	}
+	
+	@RequestMapping(value="/lottery", method = {RequestMethod.GET})
+	public List<String> getPhoneList(){
+		return autoLotteryService.getPhoneList();
+	}
+	
+	@RequestMapping(value="/lottery", method = {RequestMethod.POST})
+	public void addPhoneList(@RequestBody List<String> phones){
+		autoLotteryService.addPhoneList(phones);
+	}
+	
 	
 
 }
