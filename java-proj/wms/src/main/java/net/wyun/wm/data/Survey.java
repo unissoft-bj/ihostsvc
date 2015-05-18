d20bf193d63865a91b06f69f8361ffbea56fc39e
michaelyin <gt8442b@yahoo.com>
2015-05-17 20:37:23 -0400

    add mac.

--- java-proj/wms/src/main/java/net/wyun/wm/data/Survey.java
+++ java-proj/wms/src/main/java/net/wyun/wm/data/Survey.java
@@ -17,6 +17,18 @@ public class Survey {
 	Answer[] answers;
 	
 	Surveyee surveyee;
+	
+	String mac;
+	
+	
+
+	public String getMac() {
+		return mac;
+	}
+
+	public void setMac(String mac) {
+		this.mac = mac;
+	}
 
 	public Answer[] getAnswers() {
 		return answers;
