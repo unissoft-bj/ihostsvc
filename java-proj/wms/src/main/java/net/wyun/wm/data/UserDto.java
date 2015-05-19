76a836a49d45479bead32b107f67ee7fe4bb62eb
michaelyin <gt8442b@yahoo.com>
2015-05-19 00:21:49 -0400

    add debug to print out user permissions.

--- java-proj/wms/src/main/java/net/wyun/wm/data/UserDto.java
+++ java-proj/wms/src/main/java/net/wyun/wm/data/UserDto.java
@@ -4,8 +4,15 @@
 package net.wyun.wm.data;
 
 import java.io.Serializable;
+import java.util.Collection;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import net.wyun.wm.domain.Permission;
+import net.wyun.wm.domain.account.Account;
 import net.wyun.wm.domain.mac.Mac;
+import net.wyun.wm.security.WmsAuthenticationProvider;
 
 /**
  * @author Xuecheng
@@ -13,11 +20,23 @@ import net.wyun.wm.domain.mac.Mac;
  */
 public class UserDto{
 
+	private static final Logger logger = LoggerFactory.getLogger(UserDto.class);
 	public UserDto() {}
 	public UserDto(Mac mac) {
 		this.mac = mac.getMacInString();
 		this.name = mac.getMacInString();
 		this.mac_pwd = mac.getPassword();
+		
+		StringBuilder perm = new StringBuilder();
+		if(mac.getAccount() != null){
+			Account acct = mac.getAccount();
+			Collection<Permission> c = acct.getPermissions();
+			for(Permission p:c){
+				perm.append(p.getName()).append(',');
+			}
+		}
+		logger.debug("mac {} has permission: {}", mac, perm.toString());
+		
 		this.role = mac.getRole() == null?"UNKNOWN":mac.getRole().getName();
 		this.phone = mac.getAccount() == null?"UNKNOWN":mac.getAccount().getPhone();
 	}
