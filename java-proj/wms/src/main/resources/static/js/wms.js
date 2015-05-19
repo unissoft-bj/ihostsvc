c80732c4f2707f7ff8d37f74414ed041b38d5502
ycyn521 <ycyn521@qq.com>
2015-05-19 18:25:51 +0800

    2015_auto beta0.1

--- java-proj/wms/src/main/resources/static/js/wms.js
+++ java-proj/wms/src/main/resources/static/js/wms.js
@@ -1,3 +1,36 @@
+function CurentTime()
+    { 
+        var now = new Date();
+       
+        var year = now.getFullYear();       //年
+        var month = now.getMonth() + 1;     //月
+        var day = now.getDate();            //日
+       
+        var hh = now.getHours();            //时
+        var mm = now.getMinutes();          //分
+       
+        var clock = year + "-";
+       
+        if(month < 10)
+            clock += "0";
+       
+        clock += month + "-";
+       
+        if(day < 10)
+            clock += "0";
+           
+        clock += day + " ";
+       
+        if(hh < 10)
+            clock += "0";
+           
+        clock += hh + ":";
+        if (mm < 10) clock += '0'; 
+        clock += mm; 
+        return(clock+":00"); 
+    } 
+
+var answers = new Array();
 var myUIRoute = angular.module('MyWms', ['ui.router', 'ngAnimate','ngCookies']);
 myUIRoute.config(function($stateProvider, $urlRouterProvider) {
     $urlRouterProvider.otherwise("/index");
@@ -22,7 +55,7 @@ myUIRoute.config(function($stateProvider, $urlRouterProvider) {
             url: "/index",
             templateUrl: "tpls/index.htm",
             controller: function($scope,$http,$cookieStore) {
-                alert($cookieStore.get("ihost"));  
+                //alert($cookieStore.get("ihost"));  
                 $http.get("ihost").success(function(data){
                     //alert(data.uuidHex);
                     $cookieStore.put("ihost",data); 
@@ -31,18 +64,18 @@ myUIRoute.config(function($stateProvider, $urlRouterProvider) {
                 
                 $http.get("mymac").success(function(data){
                     //alert(data);
-                    //$cookieStore.put("mymac",data);
-                    $cookieStore.put("mymac","60-36-DD-FF-FF-BB");
+                    $cookieStore.put("mymac",data);
+                    //$cookieStore.put("mymac","AA-36-DD-FF-FF-BB");
                 }).error(function(){
-                    //alert("得不到mac地址");
-                    $cookieStore.put("mymac","60-36-DD-FF-FF-BB");
+                    alert("得不到mac地址");
+                    //$cookieStore.put("mymac","AA-36-DD-FF-FF-BB");
                 }); 
 
                 //自动登陆
-                    alert("自动登陆开始");
+                    //alert("自动登陆开始");
                     
                     if($cookieStore.get("user")==null){
-                        window.location.href="#/login1"; 
+                        window.location.href="#/diaocha1"; 
                     }
                     var autouser = $cookieStore.get("user");
                     //%7B%22name%22%3A%2260-36-DD-FF-FF-BB%22%2C%22mac%22%3A%2260-36-DD-FF-FF-BB%22%2C%22mac_pwd%22%3A%222227b540-9079-4141-9f79-ea811bba85ab%22%2C%22role%22%3A%22UNKNOWN%22%2C%22phone%22%3A%222006548878%22%7D
@@ -53,10 +86,10 @@ myUIRoute.config(function($stateProvider, $urlRouterProvider) {
                         "content-type" : "application/x-www-form-urlencoded"
                         }
                     }).success(function(response) {                    
-                        window.location.href="#/main";                    
+                        window.location.href="#/diaocha_over";                    
                     }).error(function(response) {
                         alert("非法操作");
-                        window.location.href="#/login1"; 
+                        window.location.href="#/diaocha1"; 
                         //alert(response);
                         if (response.code=="phone_login") {
                             alert("phone_login");
@@ -86,7 +119,7 @@ myUIRoute.config(function($stateProvider, $urlRouterProvider) {
                 })
                 .error(function() {
                      alert("非法登陆");
-                     window.location.href="#/login1"; 
+                     window.location.href="#/login2"; 
                 });
 
                 $scope.logout = function(){
@@ -139,14 +172,44 @@ myUIRoute.config(function($stateProvider, $urlRouterProvider) {
         .state('login2', {
             url: "/login2",
             templateUrl: "tpls/login2.htm",
-            controller:function($scope,$http){
-                $scope.formData = {"mac":"C3-A1-C3-C4-C5-C6"};
+            controller:function($scope,$http,$cookieStore){
+                var autouser = $cookieStore.get("user");
+                    //%7B%22name%22%3A%2260-36-DD-FF-FF-BB%22%2C%22mac%22%3A%2260-36-DD-FF-FF-BB%22%2C%22mac_pwd%22%3A%222227b540-9079-4141-9f79-ea811bba85ab%22%2C%22role%22%3A%22UNKNOWN%22%2C%22phone%22%3A%222006548878%22%7D
+                    var loginData = "username="+autouser.mac+"&password="+autouser.mac_pwd;
+                    
+                    $http.post("login",loginData,{
+                        headers : {
+                        "content-type" : "application/x-www-form-urlencoded"
+                        }
+                    }).success(function(response) {
+                        if(autouser.role=="ROLE_MANAGER"){
+                            window.location.href="#/main"; 
+                        }                    
+                                           
+                    }).error(function(response) {
+                        alert("请输入上网码");
+                        
+                        
+                    });
+
+
+                $http.get("mymac").success(function(data){
+                    //alert(data);
+                    $cookieStore.put("mymac",data);
+                    //$cookieStore.put("mymac","AA-BB-DD-FF-FF-BB");
+                }).error(function(){
+                    alert("得不到mac地址");
+                    //$cookieStore.put("mymac","AA-BB-DD-FF-FF-BB");
+                });
+                $scope.formData = {};
+                $scope.formData.mac = $cookieStore.get("mymac");
                 $scope.msg="请输入上网码";
                 $scope.register = function(){
                     // alert($scope.formData.shangwangma);
                     $http.post("register",$scope.formData)
                     .success(function(response) {
-
+                        console.log(response);
+                        $cookieStore.put("user",response);
                         window.location.href="#/main";
                    
                     })
@@ -220,9 +283,15 @@ myUIRoute.config(function($stateProvider, $urlRouterProvider) {
             url: "/diaocha1",
             templateUrl: "tpls/diaocha1.htm",
             controller:function($scope,$http){
-                $scope.formData = {};            
+                $scope.formData = {"q_id":1};            
                 $scope.login = function(){
-                     //alert($scope.formData.phone_pass);
+                    if($scope.formData.available_option==null){
+                        alert("请至少选择一项");
+                        return false;
+                    }
+                    $scope.formData.available_option = JSON.stringify($scope.formData.available_option);
+                    answers.push($scope.formData);
+                    console.log(answers);
                     window.location.href="#/diaocha2";
                     
                 }
@@ -233,9 +302,18 @@ myUIRoute.config(function($stateProvider, $urlRouterProvider) {
             url: "/diaocha2",
             templateUrl: "tpls/diaocha2.htm",
             controller:function($scope,$http){
-                $scope.formData = {};            
+
+                $scope.formData = {"q_id":2};
+                console.log($scope.formData.available_option)            
                 $scope.login = function(){
-                     //alert($scope.formData.phone_pass);
+                    if($scope.formData.available_option==null){
+                        alert("请至少选择一项");
+                        return false;
+                    }
+                    console.log($scope.formData.available_option)
+                    $scope.formData.available_option = JSON.stringify($scope.formData.available_option);
+                    answers.push($scope.formData);
+                    console.log(answers);
                     window.location.href="#/diaocha3";
                     
                 }
@@ -246,9 +324,17 @@ myUIRoute.config(function($stateProvider, $urlRouterProvider) {
             url: "/diaocha3",
             templateUrl: "tpls/diaocha3.htm",
             controller:function($scope,$http){
-                $scope.formData = {};            
+                $scope.formData = {"q_id":3};
+                console.log($scope.formData.available_option)            
                 $scope.login = function(){
-                     //alert($scope.formData.phone_pass);
+                    if($scope.formData.available_option==null){
+                        alert("请至少选择一项");
+                        return false;
+                    }
+                    console.log($scope.formData.available_option)
+                    $scope.formData.available_option = JSON.stringify($scope.formData.available_option);
+                    answers.push($scope.formData);
+                    console.log(answers);
                     window.location.href="#/diaocha4";
                     
                 }
@@ -259,9 +345,18 @@ myUIRoute.config(function($stateProvider, $urlRouterProvider) {
             url: "/diaocha4",
             templateUrl: "tpls/diaocha4.htm",
             controller:function($scope,$http){
-                $scope.formData = {};            
+                $scope.formData = {"q_id":4};
+                $scope.formData.available_option=null;
+                console.log($scope.formData.available_option)            
                 $scope.login = function(){
-                     //alert($scope.formData.phone_pass);
+                    if($scope.formData.available_option==null){
+                        alert("请答题");
+                        return false;
+                    }
+                    console.log($scope.formData.available_option)
+                    $scope.formData.available_option = JSON.stringify($scope.formData.available_option);
+                    answers.push($scope.formData);
+                    console.log(answers);
                     window.location.href="#/diaocha5";
                     
                 }
@@ -272,9 +367,17 @@ myUIRoute.config(function($stateProvider, $urlRouterProvider) {
             url: "/diaocha5",
             templateUrl: "tpls/diaocha5.htm",
             controller:function($scope,$http){
-                $scope.formData = {};            
+                $scope.formData = {"q_id":5};
+                console.log($scope.formData.available_option)            
                 $scope.login = function(){
-                     //alert($scope.formData.phone_pass);
+                    if($scope.formData.available_option==null){
+                        alert("请至少选择一项");
+                        return false;
+                    }
+                    console.log($scope.formData.available_option);
+                    $scope.formData.available_option = JSON.stringify($scope.formData.available_option);
+                    answers.push($scope.formData);
+                    console.log(answers);
                     window.location.href="#/diaocha6";
                     
                 }
@@ -284,14 +387,168 @@ myUIRoute.config(function($stateProvider, $urlRouterProvider) {
         .state('diaocha6', {
             url: "/diaocha6",
             templateUrl: "tpls/diaocha6.htm",
-            controller:function($scope,$http){
-                $scope.formData = {};            
+            controller:function($scope,$http,$cookieStore){
+                $scope.formData = {};
+                $scope.formData.show_location= "Boston";
+                console.log($scope.formData.available_option)
                 $scope.login = function(){
-                     //alert($scope.formData.phone_pass);
-                    window.location.href="#/diaocha6";
+                    if($scope.formData.name==null){
+                        alert("请输入姓名");
+                        return false;
+                    }
+                    if($scope.formData.age==null){
+                        alert("请输入年龄");
+                        return false;
+                    }
+                    if($scope.formData.gender==null){
+                        alert("请选择性别");
+                        return false;
+                    }
+                    if($scope.formData.city==null){
+                        alert("请输入所在城市");
+                        return false;
+                    }
+                    if($scope.formData.phone==null){
+                        alert("请输入手机号");
+                        return false;
+                    }
+                    if($scope.formData.has_car==null){
+                        alert("请选择是否有私家车");
+                        return false;
+                    }
+                    console.log($scope.formData)
+                    //answers.push($scope.formData);
+                    console.log(answers);
+
+                    var postData = {};
+                    postData.answers = answers;
+                    
+                    postData.surveyee = $scope.formData;
+                    postData.mac = $cookieStore.get("mymac");
+
+                    console.log(postData);
+
+                    $http.post("survey",postData)
+                    .success(function(response) {
+
+                        console.log(response);
+                        $cookieStore.put("user",response);
+                        window.location.href="#/index";
+                   
+                    })
+                    .error(function() {
+                        alert("未知异常");
+                        //$scope.msg="登陆失败，请重新输入上网码";
+                        //window.location.href="#/login2";
+                    });
+                    //window.location.href="#/diaocha_over";
                     
                 }
             }
+        })
+
+        .state('diaocha_over', {
+            url: "/diaocha_over",
+            templateUrl: "tpls/diaocha_over.htm",
+            
+        })
+
+        //设置带宽
+        .state('daikuan', {
+            url: "/daikuan",
+            templateUrl: "tpls/daikuan.htm",
+            controller:function($scope,$http){
+                $scope.formData = {};            
+                $scope.setDaikuan = function(){
+                     alert("设置带宽");
+                    
+                    
+                }
+            }
+        })
+        //设置奖池时间
+        .state('lotteryTime', {
+            url: "/lotteryTime",
+            templateUrl: "tpls/lotteryTime.htm",
+            controller:function($scope,$http,$cookieStore){
+                $scope.formData = {};
+                if($cookieStore.get("start_t")==null){
+                    $scope.formData.start_t = CurentTime();
+                }else{
+                    $scope.formData.start_t = $cookieStore.get("start_t");
+                }
+                
+                if($cookieStore.get("end_t")==null){
+                    $scope.formData.end_t = CurentTime();
+                }else{
+                    $scope.formData.end_t = $cookieStore.get("end_t");
+                }
+
+                
+                $scope.msg="请按照指定格式输入起止时间";
+                $scope.setTime = function(){
+                    $cookieStore.put("start_t",$scope.formData.start_t);
+                    $cookieStore.put("end_t",$scope.formData.end_t);
+                    
+                    $http.post("secure/lottery/time",$scope.formData)
+                    .success(function(response) {
+                        console.log(response); 
+                        $scope.msg="设置成功";                 
+                    })
+                    .error(function() {
+                        alert("未知异常");
+                        $scope.msg="设置失败，请重新登录";
+                        //$scope.msg="登陆失败，请重新输入上网码";
+                        //window.location.href="#/login2";
+                    });
+                    
+                    
+                }
+            }
+        })
+
+        //奖池列表
+        .state('lotteryList', {
+            url: "/lotteryList",
+            templateUrl: "tpls/lotteryList.htm",
+            controller:function($scope,$http,$cookieStore){
+                
+                $http.get("secure/lottery")
+                .success(function(response){
+                    $scope.lottery = response;
+                    console.log($scope.lottery);
+                }).error(function(){
+                    alert("获取奖池失败");
+                });
+                    
+                    
+            }
+            
+        })
+
+        //奖池添加号码
+        .state('lotteryAdd', {
+            url: "/lotteryAdd",
+            templateUrl: "tpls/lotteryAdd.htm",
+            controller:function($scope,$http,$cookieStore){
+                $scope.msg="多个手机号请用|分隔";
+                $scope.formData={};
+                $scope.addPhone = function(){
+                    console.log($scope.formData.phone);
+                    var phones = $scope.formData.phone.split("|");
+                    console.log(phones);
+
+                    $http.post("secure/lottery",phones)
+                    .success(function(response){
+                        alert("添加成功");
+                        window.location.href="#/lotteryList";
+                    }).error(function(){
+                        alert("添加失败");
+                    });
+                    }    
+                    
+            }
+            
         })
         
         ;
