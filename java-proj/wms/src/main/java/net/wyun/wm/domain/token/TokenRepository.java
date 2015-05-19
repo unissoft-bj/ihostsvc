ec1ff6e398d4b97caf29719eaaa120587dc347ef
michaelyin <gt8442b@yahoo.com>
2015-05-19 11:22:10 -0400

    add function to check the count of Token table.

--- java-proj/wms/src/main/java/net/wyun/wm/domain/token/TokenRepository.java
+++ java-proj/wms/src/main/java/net/wyun/wm/domain/token/TokenRepository.java
@@ -4,6 +4,7 @@
 package net.wyun.wm.domain.token;
 
 
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 
 /**
@@ -13,4 +14,7 @@ import org.springframework.data.repository.CrudRepository;
 public interface TokenRepository extends CrudRepository<Token, Long> {
 
 	Token findByToken(int token);
+	
+	@Query(value = "SELECT count(*) FROM token", nativeQuery = true)
+	int findCount();
 }
