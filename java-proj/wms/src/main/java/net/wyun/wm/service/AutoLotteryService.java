8cc9e39a9bec9eef2654055484c1db4676f189ff
michaelyin <gt8442b@yahoo.com>
2015-05-18 21:03:42 -0400

    add supporting functions for lottery operation.

--- java-proj/wms/src/main/java/net/wyun/wm/service/AutoLotteryService.java
+++ java-proj/wms/src/main/java/net/wyun/wm/service/AutoLotteryService.java
@@ -6,6 +6,7 @@ package net.wyun.wm.service;
 import java.util.ArrayList;
 import java.util.HashSet;
 import java.util.List;
+import java.util.Random;
 import java.util.Set;
 import java.util.regex.Pattern;
 
@@ -35,7 +36,6 @@ public class AutoLotteryService {
 
 	public void setLotteryTime(LotteryTime lotteryTime) {
 		this.lotteryTime = lotteryTime;
-		lotteryPool = lotteryPhoneRecordRepo.findByBetweenStartTAndEndTAndSelected(lotteryTime.start_t, lotteryTime.end_t, false);
 	}
 	
 	
@@ -46,12 +46,28 @@ public class AutoLotteryService {
 	List<LotteryPhoneRecord> lotteryPool = new ArrayList<LotteryPhoneRecord>();
 	
 	public List<String> getPhoneList(){
+		if(lotteryTime == null) return new ArrayList<String>();
+		
+		lotteryPool = lotteryPhoneRecordRepo.findByBetweenStartTAndEndTAndSelected(lotteryTime.start_t, lotteryTime.end_t, false);
 		Set<String> ss = new HashSet<String>();
 		for(LotteryPhoneRecord lpr:lotteryPool){
 			ss.add(lpr.getPhone());
 		}
 		
 		return new ArrayList<String>(ss);
+		
+	}
+	
+	public List<String> getPhoneAll(){
+		if(lotteryTime == null) return new ArrayList<String>();
+		
+		lotteryPool = lotteryPhoneRecordRepo.findByBetweenStartTAndEndTAndSelected(lotteryTime.start_t, lotteryTime.end_t, false);
+		List<String> all = new ArrayList<String>();
+		for(LotteryPhoneRecord lpr:lotteryPool){
+			all.add(lpr.getPhone());
+		}
+		
+		return all;
 		
 	}
 	
@@ -68,7 +84,36 @@ public class AutoLotteryService {
 		
 	}
 	
-	Pattern phonePattern = Pattern.compile("\\d{10}");
+	
+	public synchronized String pickUpLottery(){
+		//based on lotteryPool
+		int total = lotteryPool.size();
+		if(total == 0) return "";
+		
+		Random ran = new Random();
+		int ind = ran.nextInt(total);
+		
+		String lottery = lotteryPool.get(ind).getPhone();
+		
+		//get all lpr with number of lottery
+		List<LotteryPhoneRecord> toBeRemoved = new ArrayList<LotteryPhoneRecord>();
+		for(LotteryPhoneRecord lpr:lotteryPool){
+			if(lpr.getPhone().equals(lottery)){
+				toBeRemoved.add(lpr);
+				lpr.setSelected(true);
+				lotteryPhoneRecordRepo.save(lpr);
+			}
+		}
+		
+		//clean up
+		for(LotteryPhoneRecord lpr:toBeRemoved){
+			lotteryPool.remove(lpr);
+		}
+		
+		return lottery;
+	}
+	
+	Pattern phonePattern = Pattern.compile("\\d+");
 	private boolean validPhoneNum(String phone){
 		return phonePattern.matcher(phone).matches();
 	}
