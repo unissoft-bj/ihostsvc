dfd6273a8e9ff295f4d9d9c7ae27bc97b7e02459
michaelyin <gt8442b@yahoo.com>
2015-05-29 19:48:35 -0400

    add rest api to clean up testing data.

--- java-proj/wms/src/main/java/net/wyun/wm/rest/admin/AdminController.java
+++ java-proj/wms/src/main/java/net/wyun/wm/rest/admin/AdminController.java
@@ -5,18 +5,22 @@ package net.wyun.wm.rest.admin;
 
 
 import java.io.IOException;
+import java.util.Date;
 import java.util.concurrent.ExecutionException;
 import java.util.concurrent.Future;
 import java.util.concurrent.TimeUnit;
 import java.util.concurrent.TimeoutException;
 
+import net.wyun.wm.data.CleanDataRequest;
 import net.wyun.wm.data.IhostCmdRequest;
+import net.wyun.wm.service.TestDataCleanUpService;
 import net.wyun.wm.util.ProcessExecutor;
 import net.wyun.wm.util.WmsProcessExecutorHandler;
 
 import org.apache.commons.exec.CommandLine;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -52,7 +56,6 @@ public class AdminController {
 			
 		} catch (IOException | InterruptedException | ExecutionException
 				| TimeoutException e) {
-			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
 		String msg = "";
@@ -68,6 +71,23 @@ public class AdminController {
 		String json = "{'ret': '" + ret + "'," + "'msg':'" + msg + "'," + "'err':'" + err + "'";  
 		return json; 
 		
+	}
+	
+	@Autowired
+	TestDataCleanUpService testDataCleanUpService;
+	@RequestMapping(value="/clean/testdata", method=RequestMethod.POST)
+	public void cleanUpDB(@RequestBody CleanDataRequest cleanDataReq){
+		
+		int minutes = cleanDataReq.getMinutes();
+        long duration = minutes*60*1000L;
+		long cutoff = System.currentTimeMillis() - duration;
+		
+		Date cutDate = new Date(cutoff);
+		
+		logger.info("cutoff date: " + cutDate.toString());
+		
+		
+		testDataCleanUpService.clean(cutDate);
 	}
 	
 	
